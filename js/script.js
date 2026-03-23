a3f645a28833b1b78e21e6e67a4567446bce4c9f
Dileepmk <dileep.m.k126@gmail.com>
2026-03-23 19:03:13 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+(function (global) {
+
+var dc = {};
+var homeHtmlUrl = "snippets/home-snippet.html";
+var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
+
+function insertHtml(selector, html) {
+  document.querySelector(selector).innerHTML = html;
+}
+
+function showLoading(selector) {
+  insertHtml(selector, "<div class='text-center'>Loading...</div>");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(homeHtmlUrl, function (responseText) {
+    insertHtml("#main-content", responseText);
+  }, false);
+
+  document.addEventListener("click", function (event) {
+    if (event.target.closest("#specials-tile")) {
+      loadRandomCategory();
+    }
+  });
+});
+
+function loadRandomCategory() {
+  $ajaxUtils.sendGetRequest(
+    allCategoriesUrl,
+    function (response) {
+      var randomCategory = chooseRandomCategory(response);
+      var short_name = "'" + randomCategory.short_name + "'";
+      dc.loadMenuItems(short_name);
+    }
+  );
+}
+
+function chooseRandomCategory(categories) {
+  var randomIndex = Math.floor(Math.random() * categories.length);
+  return categories[randomIndex];
+}
+
+dc.loadMenuItems = function (categoryShort) {
+  alert("Loaded category: " + categoryShort);
+};
+
+global.$dc = dc;
+
+})(window);
